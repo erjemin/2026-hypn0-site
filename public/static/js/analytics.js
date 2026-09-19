110c525e40c3e3ce093fb7900768e84cd2121f8c
erjemin <erjemin@gmail.com>
2026-09-19 13:34:39 +0300

    add: добавлен Яндеккс.Метрика

--- public/static/js/analytics.js
+++ public/static/js/analytics.js
@@ -25,30 +25,25 @@
 
   // ============================================================================
   // Yandex.Metrika (Яндекс.Метрика)
-  // ID: 12345678
+  // ID: 112812760
   // ============================================================================
-  (function() {
-    window.ym = window.ym || function(){
-      (window.ym.a = window.ym.a || []).push(arguments);
-    };
-    window.ym.l = 1 * new Date();
+  (function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+  })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=112812760', 'ym');
 
-    // Загружаем скрипт Метрики
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://mc.yandex.ru/metrika/tag.js';
-    document.head.appendChild(script);
-
-    // Инициализируем Метрику
-    window.ym(12345678, 'init', {
-      trackHash: true,
-      clickmap: true,
-      referrer: document.referrer,
-      url: location.href,
-      accurateTrackBounce: true,
-      trackLinks: true
-    });
-  })();
+  window.ym(112812760, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: 'dataLayer',
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce: true,
+    trackLinks: true
+  });
 
 })();
 
